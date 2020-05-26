6cd9719db94639f5793b01e8dadaf4eb31bb046c
Changmin Choi <cmchoi9901@gmail.com>
2020-05-26 15:27:24 +0900

    remove contiguous at last

--- codes/EDVR_inference.py
+++ codes/EDVR_inference.py
@@ -76,7 +76,7 @@ class EDVRWrapper:
             output_tensor = torch.cat(output_tensor, dim=0)  # output_tensor: Tensor[T,C,H,W]
 
             # write video
-            output = output_tensor.permute(0, 2, 3, 1).contiguous()  # output: Tensor[T,H,W,C]
+            output = output_tensor.permute(0, 2, 3, 1)  # output: Tensor[T,H,W,C]
             torchvision.io.write_video(output_path, output, fps=info['video_fps'])
 
     def single_inference(self, input_tensor):
